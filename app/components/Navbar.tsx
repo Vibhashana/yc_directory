52df42224da21176ce2e26f75d4ce777d8babe55
Vibhashana <svbbk.92@gmail.com>
2024-12-10 18:14:34 +0530

    Project setup, OAuth setup using Next Auth, navbar with login & logout functionality

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import { auth, signIn, signOut } from "@/auth"
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = async () => {
+    const session = await auth();
+
+    console.log(session)
+
+    return (
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="YC Directory logo" width={144} height={30} />
+                </Link>
+                <div className="flex items-center gap-5 text-black">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+                            <form action={async () => {
+                                "use server";
+                                await signOut({ redirectTo: "/" })
+                            }}>
+                                <button type="submit">
+                                    <span>Logout</span>
+                                </button>
+                            </form>
+
+                            <Link href={`/user/id/${session?.user?.id}`}>
+                                {/* <Image
+                                    alt={`${session?.user?.name}'s image`}
+                                    src={`${session?.user?.image}`}
+                                    width={20}
+                                    height={20}
+                                    className="w-5 h-5 rounded"
+                                /> */}
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            "use server";
+
+                            await signIn('github')
+                        }}>
+                            <button type="submit">
+                                <span>Login</span>
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
