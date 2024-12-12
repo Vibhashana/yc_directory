006c0d06b17de5c383c89c07c73609bc2a2f9b72
Vibhashana <svbbk.92@gmail.com>
2024-12-12 15:03:10 +0530

    Create startup form with validations with Zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
 import { SanityLive } from "@/sanity/lib/live";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
@@ -65,6 +66,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${workSans.variable} antialiased`}>
         {children}
+        <Toaster />
         <SanityLive />
       </body>
     </html>
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -40,7 +40,19 @@ export const STARTUP_BY_ID_QUERY =
 }`);
 
 export const STARTUP_VIEWS_QUERY = defineQuery(`
-    *[_type == "startup" && _id == $id][0]{
-        _id, views
-    }
+  *[_type == "startup" && _id == $id][0]{
+      _id, views
+  }
+`);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[type == "author" && _id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+  }
 `);
