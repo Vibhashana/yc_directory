7d509fef5b2ab492865c372f7fa71f3f9130b018
Vibhashana <svbbk.92@gmail.com>
2024-12-15 21:50:58 +0530

    Fixed type errors

--- auth.ts
+++ auth.ts
@@ -1,15 +1,48 @@
-import NextAuth from "next-auth";
+import NextAuth, { JWT, Session } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
 
+// For jwt callback
+type JWTCallbackParams = {
+  token: JWT & { id?: string };
+  account: {
+    provider: string;
+    type: string;
+    providerAccountId: string;
+  } | null;
+  profile?: {
+    id: string;
+    login: string;
+    bio?: string | null;
+  };
+}
+
+// For session callback
+type SessionCallbackParams = {
+  session: Session & { id?: string };
+  token: JWT & { id?: string };
+}
+
+// @ts-ignore
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
     async signIn({
       user: { name, email, image },
       profile: { id, login, bio },
+    }: {
+      user: {
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+      };
+      profile: {
+        id: string;
+        login: string;
+        bio?: string | null;
+      };
     }) {
       const existingUser = await client
         .withConfig({ useCdn: false })
@@ -31,7 +64,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       return true;
     },
-    async jwt({ token, account, profile }) {
+    async jwt({ token, account, profile }: JWTCallbackParams) {
       if (account && profile) {
         const user = await client
           .withConfig({ useCdn: false })
@@ -44,7 +77,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: SessionCallbackParams) {
       Object.assign(session, { id: token.id });
       return session;
     },
